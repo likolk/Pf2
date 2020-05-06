8acde09e65feb78df9814c33fd570a05e25d9b22
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2020-05-06 12:11:15 +0200

    added bits file

---
+++ Downloads/midterm-likolk-master/BitSequence.java
@@ -0,0 +1,74 @@
+public class BitSequence {
+    
+    private final boolean mostSignificantBit;
+    private final BitSequence lessSignificantBits;
+    
+
+    public BitSequence() {
+        mostSignificantBit = false;
+        lessSignificantBits = null;
+    }
+    
+    public BitSequence(final boolean mostSignificantBit) {
+        this.mostSignificantBit = mostSignificantBit;
+        this.lessSignificantBits = null;
+    }
+    
+    public BitSequence(final boolean mostSignificantBit, final BitSequence lessSignificantBits) {
+        this.mostSignificantBit = mostSignificantBit;
+        this.lessSignificantBits = lessSignificantBits;
+    }
+    
+    /**
+     * Produce a String-representation of this BitSequence.
+     * e.g., given
+     *       BitSequence b = new BitSequence(true, new BitSequence(true, new BitSequence(false)));
+     *       calling
+     *       b.toString()
+     *       should return
+     *       "110"
+     */
+    public String toString() {
+        if (BitSequence.equals(true));
+        {
+        return true; 
+    }     
+        else{
+            return false; 
+        }
+       
+    } 
+    
+    /**
+     * Count the number of ones (true bits) in this BitSequence.
+     * e.g., given
+     *       BitSequence b = new BitSequence(true, new BitSequence(true, new BitSequence(false)));
+     *       calling
+     *       b.countOnes()
+     *       should return
+     *       2
+     */
+    public int countOnes() {
+        countOnes = true.BitSequence;
+        if (BitSequence.equals(true)) {
+            if (BitSequence.equals(true)) {
+                if (BitSequence.equals(false)) {
+       
+        
+        return 2;
+    } else {
+        return;
+    }
+    
+    /**
+     * Convert the given String into the corresponding BitSequence,
+     * e.g., BitSequence.fromString("110") would be equivalent to
+     *       new BitSequence(true, new BitSequence(true, new BitSequence(false))).
+     */
+    public static BitSequence fromString(final String s) {
+        // TODO implement with recursion
+        // Hint: use s.charAt(int) and s.substring(int)
+        return null;
+    }
+    
+}
